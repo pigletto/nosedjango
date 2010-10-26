a027a96df9e8ed14e6f7043df0f499f31b9f6677
winhamwr <winhamwr@gmail.com>
2010-10-26 17:28:31 -0400

    Added imports to nosedjangotests.polls.tests init so that the normal django test runner can find the tests

---
+++ nosedjangotests/polls/tests/__init__.py
@@ -0,0 +1,17 @@
+from nosedjangotests.polls.tests.test1 import (
+    DjangoTestCase,
+    DjangoTestCase2,
+    DjangoTransactionTestCase,
+    DjangoTransactionTestCase2,
+    WithTransactionUnitTestCase,
+    WithTransactionUnitTestCase2,
+    NoTransactionUnitTestCase,
+    NoTransactionUnitTestCase2,
+    MultipleFixtureTestCase,
+)
+from nosedjangotests.polls.tests.test2 import (
+    FixtureBleed1TestCase,
+    FixtureBleed2TestCase,
+    AltersBleed1TestCase,
+    AltersBleed2TestCase,
+)
